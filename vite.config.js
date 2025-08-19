2ad68a64b586d325400e0ea59dbd0faf8580989a
jaezar <tech@broadheader.com>
2025-08-19 21:22:00 +0800

    :bug: for servers

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   base: '/',
   server: {
+    host: true, // or use '0.0.0.0'
     port: 3000
   }
 })
