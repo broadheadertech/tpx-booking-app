e52e6f85bf981261173664de3e61a0d7c49a5275
jaezar <tech@broadheader.com>
2025-08-22 15:00:53 +0800

    :bug:

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import ky from 'ky'
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api'
-// const API_BASE_URL = 'https://tpx-web-service-a26o.onrender.com/api'
+// const API_BASE_URL = 'http://127.0.0.1:8000/api'
+const API_BASE_URL = 'https://tpx-web-service-a26o.onrender.com/api'
 
 class ApiService {
   constructor() {
