501c841da6c832b6d8bdc3919a3bd06170028cbc
broadheadertech <tech@broadheader.com>
2025-12-05 13:33:57 +0800

    Development/dale (#137)
    * Update Landing.jsx
    
    * Update auth.ts
    
    * update color
    
    * Update VoucherManagement.jsx
    
    * 🐛
    
    * Update bookings.ts
    
    * Update ReportsManagement.jsx
    
    * Update bookings.ts
    
    * Update bookings.ts
    
    * Update BarberModal.jsx
    
    * ✨  voucher management
    
    * ✨ voucher permission
    
    * 💄 quick action show only for staff
    
    * ✨  added permission for delete
    
    * 🐛
    
    * Update CreateBarberModal.jsx
    
    * ✨  update barber
    
    * Update BarberDashboard.jsx
    
    * 🐛  update
    
    * ✨ update landing and added skeleton loading
    
    * ✨  update idle timeout
    
    * ✨  update landing dynamic hero
    
    * Update BookingsManagement.jsx
    
    * 🐛  removed category on landing
    
    * Update .gitignore
    
    * 🐛
    
    * 💄  update download
    
    * update
    
    * update footer landing
    
    * Update Landing.jsx
    
    * update services on barber management
    
    * update routing in customer
    
    * update
    
    * update
    
    * updates
    
    * 🐛
    
    * 🐛  update change dialog to page in custom booking
    
    * update route
    
    * Update BarberProfile.jsx
    
    * Update BarberProfile.jsx
    
    * Improve pre-selected barber flow in booking pages
    
    * update
    
    * update barber io
    
    * Update BarbersList.jsx
    
    * Update BarberProfile.jsx
    
    * 🐛  improve convex pagination
    
    * 🐛  convex optimization
    
    * update
    
    * update booking and ui desgin
    
    * update
    
    ---------
    
    Co-authored-by: Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>

--- src/pages/customer/GuestServiceBooking.jsx
+++ src/pages/customer/GuestServiceBooking.jsx
@@ -1141,7 +1141,7 @@ const GuestServiceBooking = ({ onBack }) => {
           }}
           min={new Date().toISOString().split("T")[0]}
           max={
-            new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+            new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)
               .toISOString()
               .split("T")[0]
           }
